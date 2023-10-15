2538f5c478e787ee1d076f2cdc7023636a89b203
Matias Ruonala <142801440+Runski1@users.noreply.github.com>
2023-10-15 22:44:43 +0300

    blackjack playable

--- blackjack/main.py
+++ blackjack/main.py
@@ -20,25 +20,81 @@ def draw_card():
         return card, 10
 
 
+def hit(hand, hand_options):
+    card, card_value = draw_card()
+    print("Draw: " + card)
+    hand.append(card)
+    if card_value != 0:
+        new_hand = []
+        for value in hand_options:
+            value += card_value
+            new_hand.append(value)
+        hand_options = new_hand
+    else:
+        new_hand = []
+        for value in hand_options:
+            value += 1
+            new_hand.append(value)
+            value += 10
+            new_hand.append(value)
+            hand_options = new_hand
+    return hand, hand_options
+
+
+def blackjack_checker(hand_options, who):
+    new_hand = []
+    for value in hand_options:
+        if value == 21:
+            if who == "player":
+                print("Blackjack! You won!")
+                exit()
+            else:
+                print("Dealer got blackjack. You lose.")
+        if value < 21:
+            new_hand.append(value)
+    if len(new_hand) == 0:
+        if who == "player":
+            print("You busted!")
+            exit()
+        else:
+            print("Dealer busted. You win!")
+            exit()
+    else:
+        return new_hand
+
+
+def dealers_turn(players_hand):
+    hand = []
+    possible_hands = [0]
+    hand = []
+    possible_hands = [0]
+    for iterator in range(2):  # jakaa 2 ekaa korttia
+        hand, possible_hands = hit(hand, possible_hands)
+    while max(possible_hands) < players_hand:
+        possible_hands = blackjack_checker(possible_hands, "dealer")
+        print(hand, possible_hands)
+        hand, possible_hands = hit(hand, possible_hands)
+        possible_hands = blackjack_checker(possible_hands, "dealer")
+    print("Dealer won with: " + ' '.join(hand) + " | value: " + str(max(possible_hands)))
+
+
 def main():
     print("Play Blackjack")
     hand = []
-    aces = 0
-    hand_options = []
-    hand_value = 0
+    possible_hands = [0]
+    for iterator in range(2):  # jakaa 2 ekaa korttia
+        hand, possible_hands = hit(hand, possible_hands)
     while True:
+        possible_hands = blackjack_checker(possible_hands, "player")
+        print(hand, possible_hands)
         user_input = input("Hit or Stay?").lower()
         if user_input == "h":
-            card, card_value = draw_card()
-            hand.append(card)
-            if card_value != 0:
-                if len(hand_options) == 0:
-                    hand_options.append(card_value)
-                else:
-                    for h in hand_options:
-                        h += card_value
-            else:
-                for h in hand_options:
-                    h += 1
-                hand_options.append( + 11)
+            hand, possible_hands = hit(hand, possible_hands)
+        else:
+            print(max(possible_hands), type(max(possible_hands)))
+            print("Your hand:" + ' '.join(hand) + "value: " + str(max(possible_hands)))
+            dealers_turn(max(possible_hands))
+            exit()
 
+
+main()
