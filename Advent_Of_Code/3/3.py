bad00374eebb1ea4aa3b37e9c422d4ed82cd97ba
Matias Ruonala <142801440+Runski1@users.noreply.github.com>
2023-12-18 20:38:39 +0200

    AOC3

--- Advent_Of_Code/3/3.py
+++ Advent_Of_Code/3/3.py
@@ -21,4 +21,25 @@ with open(os.getcwd() + "/input.txt", "r") as file:
 # Now that we have a list of points where a symbol is located, we need to determine if a number is positioned
 # adjacent to any of these symbols. I propose that we identify numbers, record the position of their first character,
 # and note their length in characters. This information will allow us to calculate all adjacent points.
-
+numberlist = []
+with open(os.getcwd() + "/input.txt", "r") as file:
+    for line_index, line in enumerate(file):
+        for character_index, char in enumerate(line):
+            numlength = 0
+            i = 1
+            number = ""
+            if char.isdigit():
+                numlength = 1
+                number = char
+                while True:
+                    if line[character_index + i].isdigit():
+                        numlength += 1
+                        number += line[character_index + i]
+                        i += 1
+                    else:
+                        break
+                numberlist.append([int(number), (line_index, character_index), numlength])
+# numberlist has lists [number, location_of_first_character, length]
+# if there is a character in (character_index - 1) --- (character_index + (numlength -1)) range in
+# lines (lineindex -1) --- (lineindex + 1), then number should be counted in the sum
+print(numberlist)
